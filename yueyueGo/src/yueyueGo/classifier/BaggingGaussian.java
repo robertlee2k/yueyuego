028d7127f929cea5124d8c98351a944d36d35b23
robertlee2k <torobert@qq.com>
2016-09-30 17:17:23 +0800

    0

--- yueyueGo/src/yueyueGo/classifier/BaggingGaussian.java
+++ yueyueGo/src/yueyueGo/classifier/BaggingGaussian.java
@@ -72,7 +72,7 @@ public class BaggingGaussian extends ContinousClassifier {
 		int recommendThreads=0; 
 		if (runningThreads>1){ //如果外部调用者是多线程运行
 			if (this.m_skipTrainInBacktest==false){ //如果要重新构建模型，外部线程
-				recommendThreads=1;
+				recommendThreads=2;
 			}else if (this.m_skipEvalInBacktest==false){ //如果不需要构建模型，但需要重新评估模型，那将并发数折半
 				recommendThreads=runningThreads/2;
 			}else{ //如果只需要回测，简单减一后返回。
