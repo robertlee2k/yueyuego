e8460a3095da4276e0855b1bfdcfac535bc939d5
robertlee2k <torobert@qq.com>
2016-10-04 21:18:40 +0800

    9

--- yueyueGo/src/yueyueGo/fullModel/classifier/MyNNFullModel.java
+++ yueyueGo/src/yueyueGo/fullModel/classifier/MyNNFullModel.java
@@ -19,14 +19,14 @@ public class MyNNFullModel extends MyNNClassifier {
 	@Override
 	protected void initializeParams() {
 
-		m_skipTrainInBacktest = false;
+		m_skipTrainInBacktest = true;
 		m_skipEvalInBacktest = false;
 		m_policySubGroup = new String[]{"" };
 		modelArffFormat=ArffFormatFullModel.FULLMODEL_FORMAT; //这个模型缺省是为FULLMODEL用的格式
 		
 		classifierName=ClassifyUtility.MYNN_MLP_FULLMODEL;
 //		setWorkPathAndCheck(AppContext.getNOMINAL_CLASSIFIER_DIR()+classifierName+"\\");
-		m_modelEvalFileShareMode=ModelStore.YEAR_SHARED_MODEL_AND_EVAL; //覆盖父类，设定模型和评估文件的共用模式
+		m_modelEvalFileShareMode=ModelStore.HALF_YEAR_SHARED_MODEL; //覆盖父类，设定模型和评估文件的共用模式
 		
 		m_thread=EnvConstants.CPU_CORE_NUMBER-8;
 		m_learningRate=0.3; //缺省用
