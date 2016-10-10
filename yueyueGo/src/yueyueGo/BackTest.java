3cdaf8412d4d801ff0fab75f5f93ddae11e36588
robertlee2k <torobert@qq.com>
2016-10-10 09:12:29 +0800

    99

--- yueyueGo/src/yueyueGo/BackTest.java
+++ yueyueGo/src/yueyueGo/BackTest.java
@@ -127,9 +127,9 @@ public class BackTest {
 		AdaboostClassifier nModel=new AdaboostClassifier();
 
 
-//		Instances nominalResult=testBackward(nModel);
+		Instances nominalResult=testBackward(nModel);
 		//不真正回测了，直接从以前的结果文件中加载
-		Instances nominalResult=loadBackTestResultFromFile(nModel.getIdentifyName());
+//		Instances nominalResult=loadBackTestResultFromFile(nModel.getIdentifyName());
 
 		//按连续分类器回测历史数据
 //		BaggingM5P cModel=new BaggingM5P();
