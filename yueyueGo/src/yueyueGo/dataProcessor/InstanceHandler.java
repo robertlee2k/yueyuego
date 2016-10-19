fa99f08e3231acd585065e220bee531dbc3e2307
robertlee2k <torobert@qq.com>
2016-10-19 18:08:51 +0800

    修正bug（instanceSubset问题尚遗留）

--- yueyueGo/src/yueyueGo/dataProcessor/InstanceHandler.java
+++ yueyueGo/src/yueyueGo/dataProcessor/InstanceHandler.java
@@ -8,19 +8,18 @@ public class InstanceHandler {
 	private static WekaInstanceProcessor weka_handler = null;
 	
     public static BaseInstanceProcessor getHandler(GeneralInstances dataToProcess) {
-       if(weka_handler == null) {
-    	   Object obj=new Object();
-    	   synchronized (obj) {
-    		   if(weka_handler == null){
-    			   if (dataToProcess instanceof WekaInstances) {
-    				   weka_handler = new WekaInstanceProcessor();
-    			   }else{
-    				   throw new RuntimeException("cannot find predefined instancehandler for incoming data");
-    			   }
-    		   }
-    	   }
-		}
-       return weka_handler;
+    	if (dataToProcess instanceof WekaInstances) {
+    		if(weka_handler == null) {
+    			Object obj=new Object();
+    			synchronized (obj) {
+    				if(weka_handler == null){
+    					weka_handler = new WekaInstanceProcessor();
+    				}
+    			}
+    		}
+    		return weka_handler;
+    	}else{
+    		throw new RuntimeException("cannot find predefined instancehandler for incoming data");
+    	}
     }
-
 }
