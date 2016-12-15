5ddeac3c9ac4890fd0a5c504b32421b3218ff5c7
robertlee2k <torobert@qq.com>
2016-12-15 13:44:33 +0800

    group7

--- yueyueGo/src/yueyueGo/UpdateHistoryArffFile.java
+++ yueyueGo/src/yueyueGo/UpdateHistoryArffFile.java
@@ -672,17 +672,17 @@ public class UpdateHistoryArffFile {
 	 */
 	private static GeneralInstances mergeSrcTransFiles() throws Exception,
 	IllegalStateException {
-		String sourceFilePrefix=AppContext.getC_ROOT_DIRECTORY()+"sourceData\\group6\\test_onceyield_group6all";
-		GeneralInstances fullData = loadDataFromIncrementalCSVFile(sourceFilePrefix+"2005_2006.txt");
-		GeneralInstances addData = null; //loadDataFromIncrementalCSVFile(sourceFilePrefix+"2007_2008.txt");
+		String sourceFilePrefix=AppContext.getC_ROOT_DIRECTORY()+"sourceData\\group7\\test_onceyield_group7all";
+		GeneralInstances fullData = loadDataFromIncrementalCSVFile(sourceFilePrefix+"2005_2007.txt");
+		GeneralInstances addData = loadDataFromIncrementalCSVFile(sourceFilePrefix+"2008_2009.txt");
 		BaseInstanceProcessor instanceProcessor=InstanceHandler.getHandler(fullData);
-//		fullData=instanceProcessor.mergeTwoInstances(fullData, addData);
-//		System.out.println("merged one File,now row : "+ fullData.numInstances() + " column:"+ fullData.numAttributes());
-//		addData = loadDataFromIncrementalCSVFile(sourceFilePrefix+"2009_2010.txt");
-//		fullData=instanceProcessor.mergeTwoInstances(fullData, addData);
-//		System.out.println("merged one File,now row : "+ fullData.numInstances() + " column:"+ fullData.numAttributes());
+		fullData=instanceProcessor.mergeTwoInstances(fullData, addData);
+		System.out.println("merged one File,now row : "+ fullData.numInstances() + " column:"+ fullData.numAttributes());
+		addData = loadDataFromIncrementalCSVFile(sourceFilePrefix+"2010_2012.txt");
+		fullData=instanceProcessor.mergeTwoInstances(fullData, addData);
+		System.out.println("merged one File,now row : "+ fullData.numInstances() + " column:"+ fullData.numAttributes());
 
-		int startYear=2007;
+		int startYear=2013;
 		int endYear=2015;
 		for (int i=startYear;i<=endYear;i=i+2){
 			String fName=sourceFilePrefix+i+"_"+(i+1)+".txt";
