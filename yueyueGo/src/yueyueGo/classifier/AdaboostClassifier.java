ef8411a8e8cdad1e065035c078f5ed0ffe5fc8a2
robertlee2k <torobert@qq.com>
2016-12-18 13:47:17 +0800

    8

--- yueyueGo/src/yueyueGo/classifier/AdaboostClassifier.java
+++ yueyueGo/src/yueyueGo/classifier/AdaboostClassifier.java
@@ -110,7 +110,7 @@ public class AdaboostClassifier extends NominalClassifier {
 		divided=300; //将trainingData分成多少份
 		boost_iteration=10; 	//boost特有参数
 		
-		m_noCaculationAttrib=true;//false; //尝试不使用计算字段 20161215
+		m_noCaculationAttrib=false;//false; //使用计算字段 20161215
 	}
 		
 	@Override
