becd804725fc1cb6ecf7ff6c8b75ef955fd88132
robertlee2k <torobert@qq.com>
2016-12-28 16:50:09 +0800

    8888

--- yueyueGo/src/yueyueGo/classifier/BaggingM5P.java
+++ yueyueGo/src/yueyueGo/classifier/BaggingM5P.java
@@ -136,7 +136,7 @@ public class BaggingM5P extends ContinousClassifier implements ParrallelizedRunn
 		divided=300; //将trainingData分成多少份
 		
 		m_noCaculationAttrib=true;//不使用计算字段 (20161215试过无计算字段，效果不如有计算字段好） 
-		m_usePCA=true; //20121223尝试不使用PCA，效果一般，恢复PCA
+		m_usePCA=false; //20121223尝试不使用PCA
 		m_removeSWData=true; //20161222尝试不用申万行业数据
 		m_modelDataSplitMode=USE_HALF_YEAR_DATA_FOR_EVAL; //尝试评估区间使用半年数据
 	}
