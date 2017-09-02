256470f867ef8ce10610e6b117f0da2939ad724b
robertlee2k <torobert@qq.com>
2017-09-02 13:54:12 +0800

    0000

--- yueyueGo/src/yueyueGo/BackTest.java
+++ yueyueGo/src/yueyueGo/BackTest.java
@@ -95,7 +95,7 @@ public class BackTest {
 			worker.init();
 
 			//调用回测函数回测
-//			worker.callRebuildModels();
+			worker.callRebuildModels();
 			worker.callReEvaluateModels();
 //			worker.callTestBack();
 //			worker.callRefreshModelUseLatestData();
