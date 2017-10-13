7ba37123ff3db0a8f5166ba6ecb5cafcd2f8423b
robertlee2k <torobert@qq.com>
2017-10-13 18:05:22 +0800

    0000

--- yueyueGo/src/yueyueGo/BackTest.java
+++ yueyueGo/src/yueyueGo/BackTest.java
@@ -93,10 +93,10 @@ public class BackTest {
 			worker.init();
 
 			// 调用回测函数回测
-//			 worker.callRebuildModels();
-//			worker.callReEvaluateModels();
-//			 worker.callTestBack();
-			 worker.callRefreshModelUseLatestData();
+			worker.callRebuildModels();
+			worker.callReEvaluateModels();
+			// worker.callTestBack();
+			// worker.callRefreshModelUseLatestData();
 
 			// worker.callDataAnlysis();
 			// worker.testForModelStore();
@@ -133,7 +133,7 @@ public class BackTest {
 		// 逐次构建新的模型
 		model = AdaboostClassifier.initModel(m_currentArffFormat, AbstractModel.FOR_BUILD_MODEL);
 		testBackward(model);
-	
+
 		model = BaggingM5P.initModel(m_currentArffFormat, AbstractModel.FOR_BUILD_MODEL);
 		testBackward(model);
 
@@ -160,8 +160,8 @@ public class BackTest {
 	 */
 	protected void callRebuildModels() throws Exception {
 
-//		m_handSetSplitYear=
-//		 new String[] {
+		// m_handSetSplitYear=
+		// new String[] {
 		// 为年度模型使用
 		// "2008","2009","2010","2011","2012","2013","2014","2015","2016","2017",
 		// 为半年度模型使用
@@ -171,7 +171,7 @@ public class BackTest {
 		// 为月度模型使用
 		// "200801","200802","200803","200804","200805","200806","200807","200808","200809","200810","200811","200812","200901","200902","200903","200904","200905","200906","200907","200908","200909","200910","200911","200912","201001","201002","201003","201004","201005","201006","201007","201008","201009","201010","201011","201012","201101","201102","201103","201104","201105","201106","201107","201108","201109","201110","201111","201112","201201","201202","201203","201204","201205","201206","201207","201208","201209","201210","201211","201212","201301","201302","201303","201304","201305","201306","201307","201308","201309","201310","201311","201312","201401","201402","201403","201404","201405","201406","201407","201408","201409","201410","201411","201412","201501","201502","201503","201504","201505","201506","201507","201508","201509","201510","201511","201512","201601","201602","201603",
 		// "201604","201605","201606","201607","201608","201609","201610","201611"
-//		 };
+		// };
 
 		// 按连续分类器回测历史数据
 		BaggingM5P cModel = BaggingM5P.initModel(m_currentArffFormat, AbstractModel.FOR_BUILD_MODEL);
@@ -225,13 +225,15 @@ public class BackTest {
 		AdaboostClassifier nModel = AdaboostClassifier.initModel(m_currentArffFormat, AbstractModel.FOR_BACKTEST_MODEL);
 		ResultsHolder nominalResult = testBackward(nModel);
 		// 不真正回测了，直接从以前的结果文件中加载
-//		GeneralInstances nominalResult=loadBackTestResultFromFile(nModel.getIdentifyName());
+		// GeneralInstances
+		// nominalResult=loadBackTestResultFromFile(nModel.getIdentifyName());
 
 		// 按连续分类器回测历史数据
 		BaggingM5P cModel = BaggingM5P.initModel(m_currentArffFormat, AbstractModel.FOR_BACKTEST_MODEL);
 		ResultsHolder continuousResult = testBackward(cModel);
 		// 不真正回测了，直接从以前的结果文件中加载
-//		 GeneralInstances continuousResult=loadBackTestResultFromFile(cModel.getIdentifyName());
+		// GeneralInstances
+		// continuousResult=loadBackTestResultFromFile(cModel.getIdentifyName());
 
 		saveResultsAndStatistics(nModel, nominalResult, cModel, continuousResult);
 
@@ -302,7 +304,7 @@ public class BackTest {
 				BaseInstanceProcessor instanceProcessor = InstanceHandler.getHandler(fullSetData);
 				// 准备输出数据格式
 				if (result == null) {// initialize result instances
-					result = new ResultsHolder(clModel, fullSetData,m_currentArffFormat);
+					result = new ResultsHolder(clModel, fullSetData, m_currentArffFormat);
 				}
 				int policyIndex = BaseInstanceProcessor.findATTPosition(fullSetData,
 						m_currentArffFormat.m_policy_group);
@@ -333,7 +335,7 @@ public class BackTest {
 				}
 
 				GeneralInstances evaluationData = null;
-				if (clModel.is_skipEvalInBacktest() == false || clModel.is_skipTrainInBacktest() == false) {// 如果需要评估模型，则取评估数据（训练时缺省要做一次评估）
+				if (clModel.is_skipEvalInBacktest() == false) {// 如果需要评估模型，则取评估数据
 					System.out.println("start to split evaluation set from  data: " + splitEvalClause);
 					evaluationData = instanceProcessor.getInstancesSubset(fullSetData, splitEvalClause);
 					// 对于二分类器，这里要把输入的收益率转换为分类变量
@@ -347,24 +349,20 @@ public class BackTest {
 				}
 
 				GeneralInstances testingData = null;
-				// prepare testing data
-				System.out.println("start to split testing set: " + splitTestClause);
-				testingData = instanceProcessor.getInstancesSubset(fullSetData, splitTestClause);
-				// 处理testingData：注意，预测数据的处理方式不大一样，需要保留ID和tradeDate，由预测方法内部处理
-				
-//				testingData = instanceProcessor.removeAttribs(testingData, ""+ArffFormat.YEAR_MONTH_INDEX);
-//				 //对于二分类器，这里要把输入的收益率转换为分类变量------预测数据中不需要这个了
-//				 if (clModel instanceof NominalModel ){
-//					 testingData=((NominalModel)clModel).processDataForNominalClassifier(testingData);
-//				 }
+				if (clModel.is_skipPredictInBacktest() == false) {// 如果需要预测，则取评估数据
+					// prepare testing data
+					System.out.println("start to split testing set: " + splitTestClause);
+					testingData = instanceProcessor.getInstancesSubset(fullSetData, splitTestClause);
+					// 处理testingData：注意，预测数据的处理方式不大一样，需要保留ID和tradeDate，由预测方法内部处理
 
-				System.out.println(" testing raw data size , row : " + testingData.numInstances() + " column: "
-						+ testingData.numAttributes());
+					System.out.println(" testing raw data size , row : " + testingData.numInstances() + " column: "
+							+ testingData.numAttributes());
+				}
 
-//				// 在不够强的机器上做模型训练时释放内存，改为每次从硬盘加载的方式
-//				if (clModel.is_skipTrainInBacktest() == false) {
-//					fullSetData = null; // 释放内存
-//				}
+				// // 在不够强的机器上做模型训练时释放内存，改为每次从硬盘加载的方式
+				// if (clModel.is_skipTrainInBacktest() == false) {
+				// fullSetData = null; // 释放内存
+				// }
 
 				if (threadPool != null) { // 需要多线程并发
 					// 多线程的时候clone一个clModel执行任务，当前的Model继续走下去。
@@ -409,7 +407,6 @@ public class BackTest {
 			System.out.println(" ");
 		} // end for (int i
 
-
 		if (threadPool != null) { // 需要多线程并发
 			// 全部线程已放入线程池，关闭线程池的入口。
 			threadPool.shutdown();
@@ -443,7 +440,7 @@ public class BackTest {
 				m_backtest_result_dir + m_currentArffFormat.m_data_file_prefix + "-" + clModel.getIdentifyName()
 						+ "-dailySummary.csv",
 				modelSummaries.getDailyHeader() + modelSummaries.getDailySummary(), "GBK");
-		
+
 		System.out.println(clModel.getIdentifyName() + " test result file saved.");
 		return result;
 	}
@@ -507,7 +504,8 @@ public class BackTest {
 	}
 
 	// 合并格式，子类中可覆盖
-	protected GeneralInstances mergeResultWithData(ResultsHolder  resultData, ResultsHolder referenceData, int format) throws Exception {
+	protected GeneralInstances mergeResultWithData(ResultsHolder resultData, ResultsHolder referenceData, int format)
+			throws Exception {
 		// 读取磁盘上预先保存的左侧数据
 		GeneralInstances left = null;
 
@@ -520,7 +518,7 @@ public class BackTest {
 		left = DataIOHandler.getSuppier()
 				.loadDataFromFile(AppContext.getC_ROOT_DIRECTORY() + m_currentArffFormat.getLeftDataFileName());
 
-		GeneralInstances mergedResult = resultData.mergeResults(referenceData,  left);
+		GeneralInstances mergedResult = resultData.mergeResults(referenceData, left);
 
 		// 返回结果之前需要按TradeDate重新排序
 		int tradeDateIndex = BaseInstanceProcessor.findATTPosition(mergedResult, ArffFormat.TRADE_DATE);
@@ -557,8 +555,6 @@ public class BackTest {
 				+ classiferName + RESULT_EXTENSION);
 
 	}
-
-	
 
 	// protected void testForModelStore(){
 	// AdaboostClassifier nModel=new AdaboostClassifier();
@@ -606,10 +602,12 @@ public class BackTest {
 		ShouyilvDescriptiveList[] marketTrendAnalysis = new ShouyilvDescriptiveList[3];
 		ShouyilvDescriptiveList[] monthlyShouyilvAnalysis = new ShouyilvDescriptiveList[3];
 
-		marketTrendAnalysis[0] = DataAnalysis.analyzeByMarketTrend(ShouyilvDescriptiveList.ORIGINAL_DATA, m_startYear + "01", m_endYearMonth,
-				m_currentArffFormat.m_policy_group, targetPolicies, continuousResult.getResultInstances());
-		monthlyShouyilvAnalysis[0] = DataAnalysis.analyzeByMonth(ShouyilvDescriptiveList.ORIGINAL_DATA, m_startYear, m_endYearMonth,
-				m_currentArffFormat.m_policy_group, targetPolicies, continuousResult.getResultInstances());
+		marketTrendAnalysis[0] = DataAnalysis.analyzeByMarketTrend(ShouyilvDescriptiveList.ORIGINAL_DATA,
+				m_startYear + "01", m_endYearMonth, m_currentArffFormat.m_policy_group, targetPolicies,
+				continuousResult.getResultInstances());
+		monthlyShouyilvAnalysis[0] = DataAnalysis.analyzeByMonth(ShouyilvDescriptiveList.ORIGINAL_DATA, m_startYear,
+				m_endYearMonth, m_currentArffFormat.m_policy_group, targetPolicies,
+				continuousResult.getResultInstances());
 
 		// 输出连续分类器的收益率分析统计结果
 		saveStatisiticsForOneModel(cModel, continuousResult, nModel, nominalResult, targetPolicies, marketTrendAnalysis,
@@ -647,29 +645,31 @@ public class BackTest {
 		// 输出主分类器的收益率分析统计结果
 		System.out.println("\r\n-----now output main model predictions----------" + mainModel.getIdentifyName());
 		GeneralInstances selectedInstances = ResultsHolder.returnSelectedInstances(mainResult.getResultInstances());
-		marketTrendAnalysis[1] = DataAnalysis.analyzeByMarketTrend("单模型选股:"+mainModel.getIdentifyName(), m_startYear + "01", m_endYearMonth,
-				m_currentArffFormat.m_policy_group, targetPolicies, selectedInstances);
-		monthlyShouyilvAnalysis[1] = DataAnalysis.analyzeByMonth("单模型选股:"+mainModel.getIdentifyName(), m_startYear, m_endYearMonth,
-				m_currentArffFormat.m_policy_group, targetPolicies, selectedInstances);
+		marketTrendAnalysis[1] = DataAnalysis.analyzeByMarketTrend("单模型选股:" + mainModel.getIdentifyName(),
+				m_startYear + "01", m_endYearMonth, m_currentArffFormat.m_policy_group, targetPolicies,
+				selectedInstances);
+		monthlyShouyilvAnalysis[1] = DataAnalysis.analyzeByMonth("单模型选股:" + mainModel.getIdentifyName(), m_startYear,
+				m_endYearMonth, m_currentArffFormat.m_policy_group, targetPolicies, selectedInstances);
 
 		// 用另一个模型合并选股
-		GeneralInstances classifyResult = mergeResultWithData(mainResult, secondaryResult ,
+		GeneralInstances classifyResult = mergeResultWithData(mainResult, secondaryResult,
 				mainModel.getModelArffFormat());
 		selectedInstances = ResultsHolder.returnSelectedInstances(classifyResult);
 		System.out.println("--filtered by secondary classifier:( " + secondaryModel.getIdentifyName() + ")");
-		marketTrendAnalysis[2] = DataAnalysis.analyzeByMarketTrend("双模型选股:"+mainModel.getIdentifyName(), m_startYear + "01", m_endYearMonth,
-				m_currentArffFormat.m_policy_group, targetPolicies, selectedInstances);
-		monthlyShouyilvAnalysis[2] = DataAnalysis.analyzeByMonth("双模型选股:"+mainModel.getIdentifyName(), m_startYear, m_endYearMonth,
-				m_currentArffFormat.m_policy_group, targetPolicies, selectedInstances);
+		marketTrendAnalysis[2] = DataAnalysis.analyzeByMarketTrend("双模型选股:" + mainModel.getIdentifyName(),
+				m_startYear + "01", m_endYearMonth, m_currentArffFormat.m_policy_group, targetPolicies,
+				selectedInstances);
+		monthlyShouyilvAnalysis[2] = DataAnalysis.analyzeByMonth("双模型选股:" + mainModel.getIdentifyName(), m_startYear,
+				m_endYearMonth, m_currentArffFormat.m_policy_group, targetPolicies, selectedInstances);
 
 		// 输出上述收益率分析的csv文件(按市场和按月）
-		String output = ShouyilvDescriptiveList.mergeHorizontallyToCSV(marketTrendAnalysis[0],marketTrendAnalysis[1],marketTrendAnalysis[2]) + "\r\n\r\n\r\n"
-				+ cModelSummary;
+		String output = ShouyilvDescriptiveList.mergeHorizontallyToCSV(marketTrendAnalysis[0], marketTrendAnalysis[1],
+				marketTrendAnalysis[2]) + "\r\n\r\n\r\n" + cModelSummary;
 		FileUtility.write(m_backtest_result_dir + "市场牛熊区间分析-" + mainModel.getIdentifyName()
 				+ FormatUtility.getDateStringFor(0) + ".csv", output, "GBK");
 		// 按月分析
-		String monthlyOutput = ShouyilvDescriptiveList.mergeHorizontallyToCSV(monthlyShouyilvAnalysis[0],monthlyShouyilvAnalysis[1],monthlyShouyilvAnalysis[2]) + "\r\n\r\n\r\n"
-				+ cModelSummary;
+		String monthlyOutput = ShouyilvDescriptiveList.mergeHorizontallyToCSV(monthlyShouyilvAnalysis[0],
+				monthlyShouyilvAnalysis[1], monthlyShouyilvAnalysis[2]) + "\r\n\r\n\r\n" + cModelSummary;
 		FileUtility.write(m_backtest_result_dir + "按月分析-" + mainModel.getIdentifyName()
 				+ FormatUtility.getDateStringFor(0) + ".csv", monthlyOutput, "GBK");
 
@@ -810,7 +810,7 @@ public class BackTest {
 		int threadNum = 1;
 		// 先按一般的模型计算线程数
 		if (classifier.is_skipTrainInBacktest() == false) { // 模型需要训练，这个所需内存比较大
-			threadNum = 5;
+			threadNum = 8;
 		} else if (classifier.is_skipEvalInBacktest() == false) { // 模型需要评估，这个需要内存中等
 			threadNum = EnvConstants.CPU_CORE_NUMBER + EnvConstants.CPU_CORE_NUMBER / 2;
 		} else { // 只要单纯回测，这个内存无须太多，可以全开线程
